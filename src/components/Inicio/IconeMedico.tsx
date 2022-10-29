8027b35f1a8d46dba5af5cb1a1d4d0e66490881d
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-10-29 18:36:49 -0300

    Adiciona o ícone diretamente na tela de inicio

--- src/components/Inicio/IconeMedico.tsx
+++
@@ -1,9 +0,0 @@
-import { Fontisto } from '@expo/vector-icons';
-
-const IconeMedico = (): JSX.Element => {
-    return (
-      <Fontisto name="doctor" size={124} color="black" />
-    )
-}
-
-export default IconeMedico;
--- src/screens/Inicio/index.tsx
+++ src/screens/Inicio/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Fontisto } from '@expo/vector-icons';
 import styles from "./styles";
-import IconeMedico from "@components/Inicio/IconeMedico";
 
 const Inicio = (): JSX.Element => {
     return (
     <View style={styles.root}>
-      <IconeMedico />
+      <Fontisto name="doctor" size={124} color="black" />
       <Text style={styles.text}>Bem-vindo ao ProntoMed!</Text>
     </View>
     )
