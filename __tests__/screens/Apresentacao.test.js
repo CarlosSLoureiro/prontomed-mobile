b47a81e1e7c56828690d1317c840e7f332655a36
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-11-08 19:40:24 -0300

    tests: adiciona testes para a tela de apresentação

---
+++ __tests__/screens/Apresentacao.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import Apresentacao from "@screens/Apresentacao";
+import { Pagina1, Pagina2, Pagina3, Pagina4 } from '@components/Apresentacao';
+
+describe("Deve renderizar a tela de apresentação do app", () => {
+    const wrapper = shallow(<Apresentacao />);
+
+    const obterViewPorKey = (key) => {
+        return wrapper.findWhere(node => node.name() === 'View' && parseInt(node.key()) === key);
+    }
+
+    test('Deve exibir a página 1 na view com key=1', () => {
+        const view = obterViewPorKey(1);
+        expect(view.children().type()).toBe(Pagina1);
+    });
+
+    test('Deve exibir a página 2 na view com key=2', () => {
+        const view = obterViewPorKey(2);
+        expect(view.children().type()).toBe(Pagina2);
+    });
+
+    test('Deve exibir a página 3 na view com key=3', () => {
+        const view = obterViewPorKey(3);
+        expect(view.children().type()).toBe(Pagina3);
+    });
+
+    test('Deve exibir a página 4 na view com key=4', () => {
+        const view = obterViewPorKey(4);
+        expect(view.children().type()).toBe(Pagina4);
+    });
+});
