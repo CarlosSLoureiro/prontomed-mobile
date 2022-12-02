b7443a60ed8a4ca0038a294de39d733373be4667
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-01 22:36:13 -0300

    feat: configura projeto para usar typeorm

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,31 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset'
+    ],
     plugins: [
       [
         'module-resolver',
         {
           alias: {
             '@components': './src/components',
-            '@screens': './src/screens',
-            '@hooks': './src/hooks'
+            '@entities': './src/entities',
+            '@hooks': './src/hooks',
+            '@screens': './src/screens'
           }
         }
-      ]
+      ],
+      [
+        'babel-plugin-transform-typescript-metadata'
+      ],
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true
+        }
+      ],
     ]
   };
 };
