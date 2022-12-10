a579b8d6516922686910052c83bb73b024149c47
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-10 00:48:27 -0300

    chore: remove pastas duplicadas

--- src/database/repository/pacientes/interface.ts
+++
@@ -1,8 +0,0 @@
-import Paciente from '@entity/Paciente';
-
-export default interface PacientesRepositoryInterface {
-  getAll: (pagina: number) => Promise<Array<Paciente>>;
-  create: (paciente: Partial<Paciente>) => Promise<Paciente>;
-  delete: (id: number) => Promise<void>;
-  total: () => Promise<number>;
-}
