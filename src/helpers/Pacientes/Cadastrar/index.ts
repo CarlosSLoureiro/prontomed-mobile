7bc545452dc00e59afa1b9935f1df187cb969631
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-15 19:45:42 -0300

    feat: adiciona validações e testes do helper de criar paciente

--- src/helpers/Pacientes/Cadastrar/index.ts
+++ src/helpers/Pacientes/Cadastrar/index.ts
@@ -1,5 +1,6 @@
 import { Repositories } from '@database';
 import Paciente from '@entity/Paciente';
+import { Generos } from '@entity/Paciente/enums';
 import PacientesRepositoryInterface from '@repository/Pacientes/interface';
 
 export default class CadastrarPacientesHelper {
@@ -10,12 +11,35 @@ export default class CadastrarPacientesHelper {
     this.repository = repository;
   }
 
+  private valorIndefinido (valor: any): boolean {
+    return (valor === undefined);
+  }
+
   private nomeInvalido (nome: string | undefined): boolean {
-    return (nome === undefined) || (nome !== undefined && nome.length < this.tamanhoMinimoNome);
+    return (nome === undefined) || (
+      nome !== undefined &&
+      nome.length < this.tamanhoMinimoNome
+    );
   }
 
   private emailInvalido (email: string | undefined): boolean {
     return (email !== undefined && !(/\S+@\S+\.\S+/).test(email));
+  }
+
+  private telefoneInvalido (telefone: string | undefined): boolean {
+    if (telefone !== undefined) {
+      const apenasNumeros = telefone.replace(/\D/g, '');
+      return (apenasNumeros.length < 10 && apenasNumeros.length > 11);
+    } else {
+      return false;
+    }
+  }
+
+  private alturaInvalida (altura: number | undefined): boolean {
+    return (altura === undefined) || (
+      altura !== undefined &&
+      altura >= 3
+    );
   }
 
   public async executar (dados: Partial<Paciente>): Promise<Paciente> {
@@ -25,6 +49,30 @@ export default class CadastrarPacientesHelper {
 
     if (this.emailInvalido(dados.email)) {
       throw new Error('O email do paciente é inválido');
+    }
+
+    if (this.telefoneInvalido(dados.telefone)) {
+      throw new Error('O telefone do paciente é inválido');
+    }
+
+    if (this.valorIndefinido(dados.dataNascimento)) {
+      throw new Error('A data de nascimento do paciente é inválida');
+    }
+
+    if (this.valorIndefinido(dados.genero)) {
+      throw new Error('O gênero do paciente é inválido');
+    }
+
+    if (this.valorIndefinido(dados.peso)) {
+      throw new Error('O peso do paciente é inválido');
+    }
+
+    if (this.alturaInvalida(dados.altura)) {
+      throw new Error('A altura do paciente é inválida');
+    }
+
+    if (this.valorIndefinido(dados.tipoSanguineo)) {
+      throw new Error('O tipo sanguíneo do paciente é inválido');
     }
 
     const paciente = await this.repository.cadastrar(dados);
--- src/helpers/Pacientes/Cadastrar/test.ts
+++ src/helpers/Pacientes/Cadastrar/test.ts
@@ -44,4 +44,76 @@ describe('helpers > Pacientes > Cadastrar', () => {
       expect(cadastrarSpy).toHaveBeenCalledTimes(0);
     }));
   });
+
+  describe('deve retornar erro quando o telefone do paciente for inválido', () => {
+    const telefones = ['(21) 99999-99999', '(21) 99999-99', '123456'];
+
+    telefones.forEach(telefone => test(`testa com telefone > ${telefone}`, async () => {
+      const dados: Partial<Paciente> = {
+        ...factory,
+        telefone
+      };
+
+      await expect(helper.executar(dados)).rejects.toThrow('O telefone do paciente é inválido');
+      expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+    }));
+  });
+
+  test('deve retornar erro quando a data de nascimento do paciente for inválida', async () => {
+    const dataNascimento = undefined;
+    const dados: Partial<Paciente> = {
+      ...factory,
+      dataNascimento
+    };
+
+    await expect(helper.executar(dados)).rejects.toThrow('A data de nascimento do paciente é inválida');
+    expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test('deve retornar erro quando o gênero do paciente for inválido', async () => {
+    const genero = undefined;
+    const dados: Partial<Paciente> = {
+      ...factory,
+      genero
+    };
+
+    await expect(helper.executar(dados)).rejects.toThrow('O gênero do paciente é inválido');
+    expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+  });
+
+  test('deve retornar erro quando o peso do paciente for inválido', async () => {
+    const peso = undefined;
+    const dados: Partial<Paciente> = {
+      ...factory,
+      peso
+    };
+
+    await expect(helper.executar(dados)).rejects.toThrow('O peso do paciente é inválido');
+    expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+  });
+
+  describe('deve retornar erro quando a altura do paciente for inválida', () => {
+    const alturas = [undefined, 3.01, 3, 12.1];
+
+    alturas.forEach(altura => test(`testa com altura > ${altura ?? 'undefined'}`, async () => {
+      const dados: Partial<Paciente> = {
+        ...factory,
+        altura
+      };
+
+      await expect(helper.executar(dados)).rejects.toThrow('A altura do paciente é inválida');
+      expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+    }));
+  });
+
+  test('deve retornar erro quando o tipo sanguíneo do paciente for inválido', async () => {
+    const tipoSanguineo = undefined;
+    const dados: Partial<Paciente> = {
+      ...factory,
+      tipoSanguineo
+    };
+
+    await expect(helper.executar(dados)).rejects.toThrow('O tipo sanguíneo do paciente é inválido');
+    expect(cadastrarSpy).toHaveBeenCalledTimes(0);
+  });
 });
