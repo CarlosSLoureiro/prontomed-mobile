5e88be65201338886c2798f7a636ce440b326767
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-20 03:40:48 -0300

    feat: adiciona consultas repository do banco

--- src/database/repositories.ts
+++ src/database/repositories.ts
@@ -1,9 +1,11 @@
 import { DataSource } from 'typeorm';
 
+import ConsultasRepository from '@repository/Consultas';
 import PacientesRepository from '@repository/Pacientes';
 
 import { Repositories } from './types';
 
 export default (conexao: DataSource): Repositories => ({
-  pacientesRepository: new PacientesRepository(conexao)
+  pacientesRepository: new PacientesRepository(conexao),
+  consultasRepository: new ConsultasRepository(conexao)
 });
