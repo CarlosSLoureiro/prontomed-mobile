7eb8bac7527ca0c241dbaea5ca52e681db96256b
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-23 11:54:45 -0300

    refactor: ajusta tipagens da ordenação

--- src/database/repository/Pacientes/types.ts
+++ src/database/repository/Pacientes/types.ts
@@ -6,9 +6,13 @@ export interface BuscarPacientesContrato {
   tiposSanguineos: Array<TiposSanguineos>;
 }
 
+export type ValoresDeBusca = 'nome' | 'idade' | 'peso' | 'altura' | 'id' | 'totalConsultas';
+
+export type ValoresDeOrdem = 'decrescente' | 'crescente';
+
 export interface OrdenacaoPacientesContrato {
-  ordem: string;
-  chave: string;
+  ordem: ValoresDeOrdem;
+  chave: ValoresDeBusca;
 }
 
 export interface FiltrosDeBuscarPacientesContrato {
