1e4c15be43f588d8c00c1d3134b23329fd2496f5
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-26 01:24:07 -0300

    feat: adiciona observações fakes no migrations de consultas fakes

--- src/database/migrations/1671846293186-criar-consultas-fake.ts
+++ src/database/migrations/1671846293186-criar-consultas-fake.ts
@@ -2,15 +2,28 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 import Consulta from '@entity/Consulta';
 import ConsultaFactory from '@entity/Consulta/factory';
+import Observacao from '@entity/Observacao';
+import ObservacaoFactory from '@entity/Observacao/factory';
 
 const QUANTIDADE = 100;
 
 export default class CriarConsultasFake1671846293186 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
     for (let i = 0; i < QUANTIDADE; i++) {
-      await queryRunner.manager.save(
+      const consulta = await queryRunner.manager.save(
         queryRunner.manager.create<Consulta>(Consulta, new ConsultaFactory())
       );
+
+      const observacoes = Math.floor(Math.random() * 5);
+
+      for (let i = 0; i < observacoes; i++) {
+        const observacao = new ObservacaoFactory();
+        observacao.consulta = consulta;
+
+        await queryRunner.manager.save(
+          queryRunner.manager.create<Observacao>(Observacao, observacao)
+        );
+      }
     }
   }
 
