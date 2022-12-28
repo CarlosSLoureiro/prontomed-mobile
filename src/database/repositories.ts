a13fc96c5aafbb4ad61033bb1d8870f6c1aebd22
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-28 01:57:11 -0300

    feat: adiciona observações repository no contexto global

--- src/database/repositories.ts
+++ src/database/repositories.ts
@@ -1,11 +1,13 @@
 import { DataSource } from 'typeorm';
 
 import ConsultasRepository from '@repository/Consultas';
+import ObservacoesRepository from '@repository/Observacoes';
 import PacientesRepository from '@repository/Pacientes';
 
 import { Repositories } from './types';
 
 export default (conexao: DataSource): Repositories => ({
   pacientesRepository: new PacientesRepository(conexao),
-  consultasRepository: new ConsultasRepository(conexao)
+  consultasRepository: new ConsultasRepository(conexao),
+  observacoesRepository: new ObservacoesRepository(conexao)
 });
