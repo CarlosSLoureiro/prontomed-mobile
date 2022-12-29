9b082358f1b4d56b7cf3b44a0177b4a4c1394444
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-29 03:16:03 -0300

    feat: cadastra observação nas consultas ao excluir o paciente

--- src/helpers/Pacientes/Excluir/index.ts
+++ src/helpers/Pacientes/Excluir/index.ts
@@ -1,6 +1,9 @@
 import { Repositories } from '@database';
+import Consulta from '@entity/Consulta';
 import Paciente from '@entity/Paciente';
 import PacientesRepositoryInterface from '@repository/Pacientes/interface';
+
+import CadastrarObservacaoHelper from '@helpers/Observacoes/Cadastrar';
 
 export default class ExcluirPacientesHelper {
   private readonly repository: PacientesRepositoryInterface;
@@ -9,7 +12,25 @@ export default class ExcluirPacientesHelper {
     this.repository = repository;
   }
 
+  private async cadastrarObservacao (consultas: Array<Consulta>): Promise<void> {
+    try {
+      const helper = new CadastrarObservacaoHelper();
+
+      for (const consulta of consultas) {
+        const observacao = await helper.executar(consulta, {
+          mensagem: 'O paciente foi excluído'
+        });
+        consulta?.observacoes?.push(observacao);
+      }
+    } catch (e) {
+    }
+  }
+
   public async executar (paciente: Paciente): Promise<Paciente> {
-    return await this.repository.excluir(paciente);
+    const pacienteExcluido = await this.repository.excluir(paciente);
+
+    await this.cadastrarObservacao(paciente?.consultas ?? []);
+
+    return pacienteExcluido;
   }
 }
