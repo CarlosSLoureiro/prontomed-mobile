228819c514fe6812e65a1ffc0509027678c33f1a
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-12-30 19:50:03 -0300

    chore: altera nome do banco local

--- src/database/index.tsx
+++ src/database/index.tsx
@@ -15,7 +15,7 @@ export const DatabaseProvider = ({ children }: PropsWithChildren): JSX.Element =
   const conectar = useCallback(() => {
     const database = new DataSource({
       type: 'expo',
-      database: 'prontomed19.db',
+      database: 'prontomed27.db',
       driver: require('expo-sqlite'),
       entities,
       migrations,
