4baa39275c3e3217b9047a1964df274579e9a2c9
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-01-02 01:03:57 -0300

    refactor: adiciona mais um teste em agendar consultas helper

--- src/helpers/Consultas/Agendar/test.ts
+++ src/helpers/Consultas/Agendar/test.ts
@@ -1,8 +1,11 @@
 import Consulta from '@entity/Consulta';
+import ConsultaFactory from '@entity/Consulta/factory';
 import { Generos } from '@entity/Paciente/enums';
 import PacienteFactory from '@entity/Paciente/factory';
 import ConsultasRepositoryInterface from '@repository/Consultas/interface';
 import ConsultasRepositoryMock from '@repository/Consultas/mock';
+
+import ObservacoesUtils from '@utils/Observacoes';
 
 import AgendarConsultasHelper from '.';
 
@@ -11,25 +14,43 @@ import moment from 'moment';
 describe('helpers > Consultas > Agendar', () => {
   let repository: ConsultasRepositoryInterface;
   let agendarSpy: jest.SpyInstance<any>;
+  let cadastrarObservacao: jest.SpyInstance<any>;
   let obterPossivelConsultaEmConflitoSpy: jest.SpyInstance<any>;
   let helper: AgendarConsultasHelper;
 
   beforeEach(() => {
     repository = new ConsultasRepositoryMock();
     agendarSpy = jest.spyOn(repository, 'agendar');
+    cadastrarObservacao = jest.spyOn(ObservacoesUtils, 'cadastrar');
     obterPossivelConsultaEmConflitoSpy = jest.spyOn(repository, 'obterPossivelConsultaEmConflito');
     helper = new AgendarConsultasHelper(repository);
   });
 
   test('deve agendar consulta', async () => {
-    const factory = new PacienteFactory();
+    const pacienteFactory = new PacienteFactory();
     const data = new Date();
 
     obterPossivelConsultaEmConflitoSpy.mockReturnValue(undefined);
 
-    await expect(helper.executar(factory, data)).resolves.toBeInstanceOf(Consulta);
+    await expect(helper.executar(pacienteFactory, data)).resolves.toBeInstanceOf(Consulta);
     expect(obterPossivelConsultaEmConflitoSpy).toHaveBeenCalledTimes(1);
-    expect(agendarSpy).toHaveBeenCalledWith(factory, data);
+    expect(agendarSpy).toHaveBeenCalledWith(pacienteFactory, data);
+  });
+
+  test('deve cadastrar observaçõo na consulta ao agendar a consulta', async () => {
+    const pacienteFactory = new PacienteFactory();
+    const consultaFactory = new ConsultaFactory();
+    const data = new Date();
+
+    consultaFactory.paciente = pacienteFactory;
+
+    agendarSpy.mockReturnValue(consultaFactory);
+    obterPossivelConsultaEmConflitoSpy.mockReturnValue(undefined);
+
+    await expect(helper.executar(pacienteFactory, data)).resolves.toBeInstanceOf(Consulta);
+    expect(obterPossivelConsultaEmConflitoSpy).toHaveBeenCalledTimes(1);
+    expect(agendarSpy).toHaveBeenCalledWith(pacienteFactory, data);
+    expect(cadastrarObservacao).toHaveBeenCalledWith('Consulta cadastrada', [consultaFactory]);
   });
 
   test('não deve agendar consulta caso existe consulta em conflito', async () => {
