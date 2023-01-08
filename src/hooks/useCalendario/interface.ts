17f37dfca022d6cfa502c1de2b5e6cfdc6014ccf
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2023-01-08 01:01:07 -0300

    refactor: funde responsabilidades de calendário externo

--- src/hooks/useCalendario/interface.ts
+++
@@ -1,5 +0,0 @@
-import * as Calendar from 'expo-calendar';
-
-export default interface CalendarioInterface {
-  agendarConsulta: (params: Partial<Calendar.Event>) => Promise<string | undefined>;
-}
--- src/utils/Calendario/index.ts
+++ src/utils/Calendario/index.ts
@@ -1,8 +1,9 @@
+import * as expoCalendar from 'expo-calendar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Consulta from '@entity/Consulta';
 
-import Calendario from '@hooks/useCalendario';
+import Notification from '@hooks/useNotification';
 
 import { Ajustes } from '@screens/Principal/Ajustes/enums';
 
@@ -12,9 +13,90 @@ import moment from 'moment';
 
 class CalendarioUtils implements CalendarioUtilsInterface {
   private static _instancia: CalendarioUtils;
+  private readonly variavel = 'ProntoMed:ID_CALENDARIO';
 
   public static get Instancia (): CalendarioUtils {
     return this._instancia || (this._instancia = new this());
+  }
+
+  private async obterCelendario (id: string): Promise<string | null> {
+    const { status } = await expoCalendar.requestCalendarPermissionsAsync();
+    if (status === 'granted') {
+      const calendarios = await expoCalendar.getCalendarsAsync(expoCalendar.EntityTypes.EVENT);
+      const calendarioProntoMed = calendarios.filter(each => each.id === id);
+      const calendario = calendarioProntoMed[0]?.id ?? null;
+      return calendario;
+    } else {
+      Notification.error({
+        title: 'Sem acesso ao calendário!',
+        description: 'Você precisa permitir que o ProntoMed possa acessar seu calendário',
+        duration: 10000
+      });
+      return null;
+    }
+  }
+
+  private async obterCalendarioID (): Promise<string | null> {
+    const id = await AsyncStorage.getItem(this.variavel);
+
+    let calendarioId: string | null = null;
+
+    if (id !== null) {
+      calendarioId = await this.obterCelendario(id);
+    }
+
+    if (calendarioId === null) {
+      calendarioId = await this.criarCalendario();
+
+      if (calendarioId !== null) {
+        await AsyncStorage.setItem(this.variavel, calendarioId);
+      }
+    }
+
+    return calendarioId;
+  }
+
+  private async criarCalendario (): Promise<string | null> {
+    const calendarioId = await expoCalendar.createCalendarAsync({
+      title: 'ProntoMed',
+      color: 'yellow',
+      entityType: expoCalendar.EntityTypes.EVENT,
+      name: 'internalCalendarName',
+      ownerAccount: 'personal',
+      accessLevel: expoCalendar.CalendarAccessLevel.OWNER
+    });
+
+    return calendarioId ?? null;
+  }
+
+  private async removerEvento (params: Partial<expoCalendar.Event>): Promise<void> {
+    const calendarioId = await this.obterCalendarioID();
+
+    if (calendarioId === null || params.id === undefined) {
+      Notification.error({
+        title: 'Não foi possível remover a consulta!',
+        description: 'Você precisa permitir que o ProntoMed possa acessar seu calendário',
+        duration: 5000
+      });
+      return undefined;
+    }
+
+    await expoCalendar.deleteEventAsync(params.id, {});
+  }
+
+  private async agendarEvento (params: Partial<expoCalendar.Event>): Promise<string | undefined> {
+    const calendarioId = await this.obterCalendarioID();
+
+    if (calendarioId === null) {
+      Notification.error({
+        title: 'Não foi possível agendar a consulta!',
+        description: 'Você precisa permitir que o ProntoMed possa acessar seu calendário',
+        duration: 5000
+      });
+      return undefined;
+    }
+
+    return await expoCalendar.createEventAsync(calendarioId, params);
   }
 
   private async temPermissao (): Promise<boolean> {
@@ -26,7 +108,7 @@ class CalendarioUtils implements CalendarioUtilsInterface {
     const agora = new Date();
     const podeRemover = (consulta.dataAgendada > agora || forcado);
     if (permissao && podeRemover && consulta.evento !== null) {
-      await Calendario.removerConsulta({
+      await this.removerEvento({
         id: consulta.evento
       });
       return null;
@@ -40,7 +122,7 @@ class CalendarioUtils implements CalendarioUtilsInterface {
     const agora = new Date();
     const podeAgendar = (consulta.dataAgendada > agora);
     if (permissao && podeAgendar) {
-      const evento = await Calendario.agendarConsulta({
+      const evento = await this.agendarEvento({
         title: `Consulta com ${consulta.paciente.nome}`,
         startDate: consulta.dataAgendada,
         endDate: moment(consulta.dataAgendada).add(30, 'm').toDate(),
